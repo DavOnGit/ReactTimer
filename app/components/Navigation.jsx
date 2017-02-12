2b68100f93f500e0abfcd9dd66b684967619b3ec
Davide Vozzi <davide.vozzi@gmail.com>
2017-02-12 18:34:11 +0100

    Initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,33 @@
+var React = require('react')
+var {Link, IndexLink} = require('react-router')
+
+var Navigation = () => {
+  
+  return(
+    <div className='top-bar'>
+      <div className='top-bar-left'>
+        <ul className='menu'>
+          <li className='menu-text'>React Time App</li>
+          <li>
+            <IndexLink to='/' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+          </li>
+          <li>
+            <IndexLink to='/countdown' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Countdown</IndexLink>
+          </li>
+          <li>
+            <IndexLink to='/about' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
+          </li>
+        </ul>
+      </div>
+      <div className='top-bar-right'>
+        <ul className='menu'>
+          <li className='menu-text'>Created by&nbsp;
+            <a id='nav-name' href='//github.com/DavOnGit' target='_blank'>Daviz</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+module.exports = Navigation
